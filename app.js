eda07eb869f109e9239855a8bebe05e25430e400
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-01-29 01:55:32 +0500

    herp section complete

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+let elemcon = document.querySelector(".elem-container");
+let fixelem = document.querySelector(".fixed-img")
+elemcon.addEventListener("mouseenter", () => {
+    fixelem.style.display = "block"
+});
+
+elemcon.addEventListener("mouseleave", () => {
+    fixelem.style.display = "none"
+})
+
+let elems = document.querySelectorAll(".elem")
+elems.forEach((e) => {
+   e.addEventListener("mouseenter", () => {
+    let img = e.getAttribute("data-image")
+    fixelem.style.backgroundImage = `url(${img})`
+   })
+})
