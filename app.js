7e98e5ff9b83f9f373a5cea3d150608a7cc7e953
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-02-04 23:17:25 +0500

    complete clone website

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-let elemcon = document.querySelector(".elem-container");
+function page4Animation(){
+    let elemcon = document.querySelector(".elem-container");
 let fixelem = document.querySelector(".fixed-img")
 elemcon.addEventListener("mouseenter", () => {
     fixelem.style.display = "block"
@@ -19,4 +20,32 @@ elems.forEach((e) => {
     let img = e.getAttribute("data-image")
     fixelem.style.backgroundImage = `url(${img})`
    })
+})
+}
+
+function swiperanimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 50,
+      });
+}
+
+swiperanimation()
+page4Animation()
+
+let menu = document.querySelector("nav h3");
+let full = document.querySelector(".full-scr");
+let navimg = document.querySelector("nav img");
+let flag = 0;
+
+menu.addEventListener("click", function() {
+    if(flag == 0){
+        full.style.top = 0;
+        navimg.style.opacity = 0;
+        flag = 1;
+    }else{
+        full.style.top = "-100%";
+        navimg.style.opacity = 1;
+        flag = 0;
+    }
 })
