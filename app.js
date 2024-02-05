4cc268d9f415e7025c2ed6c77083898efd4f7b48
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-02-06 01:53:51 +0500

    finaly comlete fully responsive website

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function page4Animation(){
+function page4Animation() {
     let elemcon = document.querySelector(".elem-container");
-let fixelem = document.querySelector(".fixed-img")
-elemcon.addEventListener("mouseenter", () => {
-    fixelem.style.display = "block"
-});
+    let fixelem = document.querySelector(".fixed-img")
+    elemcon.addEventListener("mouseenter", () => {
+        fixelem.style.display = "block"
+    });
 
-elemcon.addEventListener("mouseleave", () => {
-    fixelem.style.display = "none"
-})
+    elemcon.addEventListener("mouseleave", () => {
+        fixelem.style.display = "none"
+    })
 
-let elems = document.querySelectorAll(".elem")
-elems.forEach((e) => {
-   e.addEventListener("mouseenter", () => {
-    let img = e.getAttribute("data-image")
-    fixelem.style.backgroundImage = `url(${img})`
-   })
-})
+    let elems = document.querySelectorAll(".elem")
+    elems.forEach((e) => {
+        e.addEventListener("mouseenter", () => {
+            let img = e.getAttribute("data-image")
+            fixelem.style.backgroundImage = `url(${img})`
+        })
+    })
 }
 
 function swiperanimation() {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: "auto",
         spaceBetween: 50,
-      });
+    });
 }
 
 swiperanimation()
@@ -38,12 +38,12 @@ let full = document.querySelector(".full-scr");
 let navimg = document.querySelector("nav img");
 let flag = 0;
 
-menu.addEventListener("click", function() {
-    if(flag == 0){
+menu.addEventListener("click", function () {
+    if (flag == 0) {
         full.style.top = 0;
         navimg.style.opacity = 0;
         flag = 1;
-    }else{
+    } else {
         full.style.top = "-100%";
         navimg.style.opacity = 1;
         flag = 0;
